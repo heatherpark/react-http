41a66ec3a8685c5e3a817ba9176dfb2c7e3ee213
Heather Park <hthr.prk@gmail.com>
2018-05-27 17:06:03 -0700

    Set up post fetching

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,6 +7,20 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    };
+
+    async componentDidMount() {
+        const posts = await this.fetchPosts();
+        this.setState({ posts });
+    }
+
+    async fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        return response.data;
+    }
+
     render () {
         return (
             <div>
